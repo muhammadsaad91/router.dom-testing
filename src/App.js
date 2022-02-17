418c09037d541a7e0b89aaaeea7a3baed70f3f1a
Muhammad SaAd <78014984+muhammadsaad91@users.noreply.github.com>
2022-02-17 19:35:37 +0500

    Router dom css and props all code for overview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import About from "./About";
 import Footer from "./Footer";
 import Input from "./Input"
 import NotFound from "./NotFound";
-import { Route, Switch } from "react-router";
+import { Switch, Route } from "react-router-dom";
 const App = ()=>{
-   return(
-<>
-{/* 
-    <Route path="/" component={NotFound} />
-    <Route path="/about" component={About} /> */}
+   return (
+      <div>
+    
+  
+          <Switch>
+            <Route  exact path="/" component={Footer} />
+            <Route  path="/a" component={Input} />
+            <Route path="/ab" component={About} />
+            <Route component={NotFound} />
 
-   <About/>
-   <Input/>
-   <NotFound/>
-</>
-  );
-
-
+          </Switch>
+      </div>
+    );
+  }
         
         
-}
+
 
 export default App;
